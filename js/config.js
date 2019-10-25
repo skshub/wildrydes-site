9f04c37beea849f78cb3116ceffc47cb366db3aa
Suraj Kumar <surajsathyapal@gmail.com>
2019-10-25 21:09:09 +0000

    Add cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_tbSUanwg2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3n1ar5po71lcib146ans2bgfuf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
